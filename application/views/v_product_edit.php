030fd2d3e88c495f408b45c4511d16970f7479a6
Isewa07 <illhamsetyow@gmail.com>
2025-09-24 23:05:37 +0700

    create edit redirect form

--- application/views/v_product_edit.php
+++ application/views/v_product_edit.php
@@ -2,7 +2,7 @@
     <div class="row mb-3">
         <label class="col-sm-2 col-form-label">Product Name</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="ProductName" name="ProductName" required>
+            <input type="text" class="form-control" id="EditProductName" name="ProductName" required>
         </div>
     </div>
 
@@ -10,7 +10,7 @@
         <label class="col-sm-2 col-form-label">Product Price</label>
         <div class="col-sm-10">
             <input type="text" oninput="this.value=this.value.replace(/[^0-9]/g,'');" 
-                class="form-control" id="ProductPrice" name="ProductPrice" required>
+                class="form-control" id="EditProductPrice" name="ProductPrice" required>
         </div>
     </div>
 
@@ -18,14 +18,14 @@
         <label class="col-sm-2 col-form-label">Product Stock</label>
         <div class="col-sm-10">
             <input type="text" oninput="this.value=this.value.replace(/[^0-9]/g,'');" 
-                class="form-control" id="ProductStock" name="ProductStock" required>
+                class="form-control" id="EditProductStock" name="ProductStock" required>
         </div>
     </div>
 
     <div class="row mb-3">
         <label class="col-sm-2 col-form-label">For Sale</label>
         <div class="col-sm-10">
-            <select id="ProductSale" name="ProductSale" class="form-control">
+            <select id="EditProductSale" name="ProductSale" class="form-control">
                 <option value="1">For Sale</option>
                 <option value="0">Not For Sale</option>
             </select>
