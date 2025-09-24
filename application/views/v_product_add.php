f08acf413260caf639e399043a1be83fd274818e
Isewa07 <illhamsetyow@gmail.com>
2025-09-25 00:13:31 +0700

    fix form layout add and edit redirect

--- application/views/v_product_add.php
+++ application/views/v_product_add.php
@@ -1,37 +1,67 @@
-<form action="<?php echo site_url('Product/saveRedirect'); ?>" method="post">
-    <div class="row mb-3">
-        <label class="col-sm-2 col-form-label">Product Name</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="ProductName" name="ProductName" required>
-        </div>
-    </div>
+<div class="row">
+    <div class="col-lg-12 mb-4">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Product</h6>
+            </div>
 
-    <div class="row mb-3">
-        <label class="col-sm-2 col-form-label">Product Price</label>
-        <div class="col-sm-10">
-            <input type="text" oninput="this.value=this.value.replace(/[^0-9]/g,'');" 
-                class="form-control" id="ProductPrice" name="ProductPrice" required>
-        </div>
-    </div>
+            <div class="card-body">
+                <form action="<?php echo site_url('Product/saveRedirect'); ?>" method="post">
+                    
+                    <!-- Product Name -->
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label">Product Name</label>
+                        <div class="col-sm-10">
+                            <input type="text" 
+                                   class="form-control" 
+                                   id="ProductName" 
+                                   name="ProductName" 
+                                   required>
+                        </div>
+                    </div>
 
-    <div class="row mb-3">
-        <label class="col-sm-2 col-form-label">Product Stock</label>
-        <div class="col-sm-10">
-            <input type="text" oninput="this.value=this.value.replace(/[^0-9]/g,'');" 
-                class="form-control" id="ProductStock" name="ProductStock" required>
-        </div>
-    </div>
+                    <!-- Product Price -->
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label">Product Price</label>
+                        <div class="col-sm-10">
+                            <input type="text" 
+                                   class="form-control" 
+                                   id="ProductPrice" 
+                                   name="ProductPrice" 
+                                   oninput="this.value=this.value.replace(/[^0-9]/g,'');" 
+                                   required>
+                        </div>
+                    </div>
 
-    <div class="row mb-3">
-        <label class="col-sm-2 col-form-label">For Sale</label>
-        <div class="col-sm-10">
-            <select id="ProductSale" name="ProductSale" class="form-control">
-                <option value="1">For Sale</option>
-                <option value="0">Not For Sale</option>
-            </select>
-        </div>
-    </div>
+                    <!-- Product Stock -->
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label">Product Stock</label>
+                        <div class="col-sm-10">
+                            <input type="text" 
+                                   class="form-control" 
+                                   id="ProductStock" 
+                                   name="ProductStock" 
+                                   oninput="this.value=this.value.replace(/[^0-9]/g,'');" 
+                                   required>
+                        </div>
+                    </div>
 
-    <button type="submit" class="btn btn-primary">Submit</button>
-    <a href="<?php echo site_url('Product/redirect_view'); ?>" class="btn btn-secondary">Cancel</a>
-</form>
+                    <!-- For Sale -->
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label">For Sale</label>
+                        <div class="col-sm-10">
+                            <select id="ProductSale" name="ProductSale" class="form-control">
+                                <option value="1">For Sale</option>
+                                <option value="0">Not For Sale</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <!-- Buttons -->
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <a href="<?php echo site_url('Product/redirect_view'); ?>" class="btn btn-secondary">Cancel</a>
+                </form>
+            </div>
+        </div>                        
+    </div>
+</div>
--- application/views/v_product_edit.php
+++ application/views/v_product_edit.php
@@ -1,48 +1,68 @@
-<form action="<?php echo site_url('Product/saveRedirect'); ?>" method="post">
-    <input type="hidden" name="ProductID" value="<?php echo isset($product['ID']) ? $product['ID'] : ''; ?>">
+<div class="row">
+    <div class="col-lg-12 mb-4">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Product</h6>
+            </div>
 
-    <div class="row mb-3">
-        <label class="col-sm-2 col-form-label">Product Name</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" ID="ProductName" 
-                   name="ProductName" 
-                   value="<?php echo isset($product['Name']) ? $product['Name'] : ''; ?>" 
-                   required>
-        </div>
+            <div class="card-body">
+                <form action="<?php echo site_url('Product/saveRedirect'); ?>" method="post">
+                    <input type="hidden" name="ProductID" value="<?php echo isset($product['ID']) ? $product['ID'] : ''; ?>">
+
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label">Product Name</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" ID="ProductName" 
+                                name="ProductName" 
+                                value="<?php echo isset($product['Name']) ? $product['Name'] : ''; ?>" 
+                                required>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label">Product Price</label>
+                        <div class="col-sm-10">
+                            <input type="text" oninput="this.value=this.value.replace(/[^0-9]/g,'');" 
+                                class="form-control" ID="ProductPrice" 
+                                name="ProductPrice" 
+                                value="<?php echo isset($product['Price']) ? $product['Price'] : ''; ?>" 
+                                required>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label">Product Stock</label>
+                        <div class="col-sm-10">
+                            <input type="text" oninput="this.value=this.value.replace(/[^0-9]/g,'');" 
+                                class="form-control" ID="ProductStock" 
+                                name="ProductStock" 
+                                value="<?php echo isset($product['Stock']) ? $product['Stock'] : ''; ?>" 
+                                required>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label">For Sale</label>
+                        <div class="col-sm-10">
+                            <select ID="ProductSale" name="ProductSale" class="form-control">
+                                <option value="1" <?php echo (isset($product['Is_sell']) && $product['Is_sell'] == 1) ? 'selected' : ''; ?>>For Sale</option>
+                                <option value="0" <?php echo (isset($product['Is_sell']) && $product['Is_sell'] == 0) ? 'selected' : ''; ?>>Not For Sale</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <a href="<?php echo site_url('Product/redirect_view'); ?>" class="btn btn-secondary">Cancel</a>
+                </form>
+            </div>
+        </div>                        
     </div>
+</div>
 
-    <div class="row mb-3">
-        <label class="col-sm-2 col-form-label">Product Price</label>
-        <div class="col-sm-10">
-            <input type="text" oninput="this.value=this.value.replace(/[^0-9]/g,'');" 
-                   class="form-control" ID="ProductPrice" 
-                   name="ProductPrice" 
-                   value="<?php echo isset($product['Price']) ? $product['Price'] : ''; ?>" 
-                   required>
-        </div>
-    </div>
 
-    <div class="row mb-3">
-        <label class="col-sm-2 col-form-label">Product Stock</label>
-        <div class="col-sm-10">
-            <input type="text" oninput="this.value=this.value.replace(/[^0-9]/g,'');" 
-                   class="form-control" ID="ProductStock" 
-                   name="ProductStock" 
-                   value="<?php echo isset($product['Stock']) ? $product['Stock'] : ''; ?>" 
-                   required>
-        </div>
-    </div>
 
-    <div class="row mb-3">
-        <label class="col-sm-2 col-form-label">For Sale</label>
-        <div class="col-sm-10">
-            <select ID="ProductSale" name="ProductSale" class="form-control">
-                <option value="1" <?php echo (isset($product['Is_sell']) && $product['Is_sell'] == 1) ? 'selected' : ''; ?>>For Sale</option>
-                <option value="0" <?php echo (isset($product['Is_sell']) && $product['Is_sell'] == 0) ? 'selected' : ''; ?>>Not For Sale</option>
-            </select>
-        </div>
-    </div>
 
-    <button type="submit" class="btn btn-primary">Submit</button>
-    <a href="<?php echo site_url('Product/redirect_view'); ?>" class="btn btn-secondary">Cancel</a>
-</form>
+
+
+
+
